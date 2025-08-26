8f3a8d3fe98ff6b1634971f4982e08a82766f456
SteveK <stevendeankennedy@users.noreply.github.com>
2025-08-26 09:59:59 -0400

    First Day
    Example code

---
+++ 0826/first_program_ever_its_totally_the_best_and_fun.py
@@ -0,0 +1,7 @@
+1+1
+3/7
+print("words")
+print(1+1)
+print("Hello, World!")
+print(3/7)
+
---
+++ 0826/simple_example.py
@@ -0,0 +1,19 @@
+# This is our first program.  It has variables.
+#   It's just an example on how to use Python.
+#   (This is called the header.)
+#
+#   SteveK & Friends
+#   8/26/2025
+
+# Set up some variables
+x = input("Please enter a number>") # Prompts the user
+y = input("Please enter another >")
+x = float(x) # Converts it to the right type
+y = float(y)
+z = x + y
+
+# Now print those out in cool ways
+#print("hello world")
+#print("hello","world")
+print("hello" + "world")
+print(x,"+",y,"=",z) # Prints out everything
