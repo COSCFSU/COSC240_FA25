9bd5e70f6396e24ecd8580e87a1c7a334d819ceb
SteveK <stevendeankennedy@users.noreply.github.com>
2025-08-28 09:34:58 -0400

    Create string_examples.py

---
+++ 0828/string_examples.py
@@ -0,0 +1,34 @@
+#This is an example that shows how some strings work
+# 8/28
+# Steve K and company
+
+print("Hello, World!")
+print("one" + "two")
+print(1+1)
+print("1"+"1")
+#print(1+"1")  # This line doesn't work
+
+#Draw a lantern
+print("====")
+print("|  |")
+print("----")
+print("|  |")
+print("====")
+
+#Print example stuff
+color = "green"
+print("My favorite color is", color)
+print("another line")
+
+print("--> these part is on the same line", end=" ")
+print("(woah!)")
+print("But sadly this line is separate again")
+print("a\nb\nc\nd\n")
+print("a\tb\tc\td\t")
+
+
+#Input example stuff
+words = input("Please type something here:")
+print(words)
+print(words+words)
+print(words+words+words)
